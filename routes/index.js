2b9cdea099fc87405a85fef42d091e4db71fe0dd
Eunice Santiago <eunicevsantiago@gmail.com>
2021-11-24 20:32:49 +0100

    bahhh

--- routes/index.js
+++ routes/index.js
@@ -11,12 +11,9 @@ module.exports = app => {
   const placeRoutes = require("./place.routes");
   app.use("/place", placeRoutes);
 
-<<<<<<< HEAD
-=======
   const pointRoutes = require("./point.routes");
   app.use("/point", pointRoutes);
 
->>>>>>> 1e6e6be53517ba90e90a72844ba43cabd80cd9a9
   const profileRoutes = require("./profile.routes");
   app.use("/profile", profileRoutes);
 
